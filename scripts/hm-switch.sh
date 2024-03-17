7da4afd2ecf46251970e88a1f306b59fbe726fbb
Tamas Mohos <zerosuxx@gmail.com>
2024-03-17 00:53:08 +0000

    improve hm switch script

--- scripts/hm-switch.sh
+++ scripts/hm-switch.sh
@@ -3,5 +3,5 @@
 target="$1"
 
 #cd $(realpath $(dirname $(dirname "${BASH_SOURCE[0]}")))
-
+export NIX_CONFIG="experimental-features = nix-command flakes"
 nix run .#home-manager -- switch --impure --flake .#$target
