ad86829f024ffd680cf0e3ecd806004ad79d9ad3
Tamas Mohos <zerosuxx@gmail.com>
2024-05-28 00:44:50 +0200

    update zsh config

--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@ let
   inherit (specialArgs) configName;
 
   bashSettings = import ./bash.nix pkgs configName;
+  zshSettings = import ./zsh.nix pkgs configName;
   gitSettings = import ./git.nix pkgs;
   packages = import ./packages.nix pkgs;
   variables = import ./variables.nix;
@@ -66,67 +67,6 @@ in
       enableZshIntegration = true;
     };
 
-    zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-
-      shellAliases = {
-        ll = "ls -alF";
-        ".." = "cd ..";
-        "..." = "cd ../..";
-        "...." = "cd ../../..";
-        g = "git";
-        k = "kubectl";
-        d = "docker";
-        dc = "docker compose";
-        gco = "git checkout";
-        gst = "git status";
-        nfl = "nix flake lock";
-        nfu = "nix flake update";
-        nflu = "nix flake lock --update-input";
-        sw = "cd ${builtins.getEnv "PWD"} && sh scripts/hm-switch.sh ${configName}";
-        hm = "home-manager";
-      };
-      
-      initExtra = ''
-        ${if builtins.hasAttr "tzdata" pkgs then ''[[ -z "$TZDIR" ]] && export TZDIR="${pkgs.tzdata}/share/zoneinfo"'' else ""}
-      '';
-
-      plugins = [
-        {
-          file = "powerlevel10k.zsh-theme";
-          name = "powerlevel10k";
-          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
-        }
-        {
-          file = "p10k.zsh";
-          name = "powerlevel10k-config";
-          src = ./dotfiles/zsh;
-        }
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.8.0";
-            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-          };
-        }
-      ];
-
-      oh-my-zsh = {
-         enable = true;
-         theme = "robbyrussell";
-         plugins = [
-           "git"
-           "npm"
-           "history"
-           "node"
-         ];
-      };
-    };
+    zsh = zshSettings;
   };
 }
---
+++ zsh.nix
@@ -0,0 +1,52 @@
+pkgs: configName: {
+  enable = true;
+  autosuggestion.enable = true;
+  syntaxHighlighting.enable = true;
+  enableCompletion = true;
+
+  shellAliases = {
+    ll = "ls -alF";
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    g = "git";
+    k = "kubectl";
+    d = "docker";
+    dc = "docker compose";
+    gco = "git checkout";
+    gst = "git status";
+    nfl = "nix flake lock";
+    nfu = "nix flake update";
+    nflu = "nix flake lock --update-input";
+    sw = "cd ${builtins.getEnv "PWD"} && sh scripts/hm-switch.sh ${configName}";
+    hm = "home-manager";
+  };
+  
+  initExtra = ''
+    ${if builtins.hasAttr "tzdata" pkgs then ''[[ -z "$TZDIR" ]] && export TZDIR="${pkgs.tzdata}/share/zoneinfo"'' else ""}
+  '';
+
+  plugins = [
+    {
+      file = "powerlevel10k.zsh-theme";
+      name = "powerlevel10k";
+      src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
+    }
+    {
+      file = "p10k.zsh";
+      name = "powerlevel10k-config";
+      src = ./dotfiles/zsh;
+    }
+  ];
+
+  oh-my-zsh = {
+     enable = true;
+     theme = "robbyrussell";
+     plugins = [
+       "git"
+       "npm"
+       "history"
+       "node"
+     ];
+  };
+}
