376cc58db0906edba5342226620517ee5fd23f27
Tamas Mohos <zerosuxx@gmail.com>
2024-08-23 16:49:50 +0200

    feat: enable k9s node shell featuregate by env

--- home/variables.nix
+++ home/variables.nix
@@ -7,8 +7,14 @@
   ...
 }: {
   home.sessionVariables = {
-    EDITOR = "nano";
+    # nix
     NIX_CONFIG = "experimental-features = nix-command flakes";
+
+    # shell
+    EDITOR = "nano";
     TZ = "Europe/Budapest";
+
+    # k9s
+    K9S_FEATURE_GATE_NODE_SHELL = "true";
   };
 }
