82da1863cf52f55a40c3a72ec14685d16b4ea4bb
Tamas Mohos <zerosuxx@gmail.com>
2025-06-13 14:07:37 +0200

    feat: add gh cli to overlays

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -3,5 +3,6 @@
 final: prev: {
   devbox = nixpkgs-unstable.legacyPackages.${system}.devbox;
   google-cloud-sdk = nixpkgs-unstable.legacyPackages.${system}.google-cloud-sdk;
+  gh = nixpkgs-unstable.legacyPackages.${system}.gh;
   terragrunt = nixpkgs-master.legacyPackages.${system}.terragrunt;
 }
