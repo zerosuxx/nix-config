64629a096ab9aabc092ebc2755d2cc2b81bef3d6
Tamas Mohos <zerosuxx@gmail.com>
2025-07-29 00:38:40 +0200

    chore: update init-hm-nix-on-droid.sh

--- scripts/init-hm-nix-on-droid.sh
+++ scripts/init-hm-nix-on-droid.sh
@@ -9,8 +9,8 @@ for pkg in $(nix-env -q); do
   echo "${pkg} package priority updated to '1'"
 done
 
-nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs
-nix-channel --update
+# nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs
+# nix-channel --update
 
 export NIX_CONFIG="experimental-features = nix-command flakes"
 nix-shell -p git openssh --run "nix run home-manager -- switch --impure --flake ."
