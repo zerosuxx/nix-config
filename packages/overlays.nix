c2215d5c7876197e802cb47fb0946d23aa5327d3
Tamas Mohos <zerosuxx@gmail.com>
2026-01-12 23:30:04 +0100

    feat: update ollama

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -6,6 +6,7 @@ final: prev: {
   google-cloud-sdk = nixpkgs-unstable.legacyPackages.${system}.google-cloud-sdk;
   k9s              = nixpkgs-unstable.legacyPackages.${system}.k9s;
   labctl           = zerosuxx-nixpkgs.packages.${system}.labctl;
+  ollama              = nixpkgs-unstable.legacyPackages.${system}.ollama;
   terraform        = zerosuxx-nixpkgs.packages.${system}.terraform;
   terragrunt       = nixpkgs-master.legacyPackages.${system}.terragrunt;
 }
