610cb4113ca31bc95d068093d50130cb8cb791a9
Tamas Mohos <zerosuxx@gmail.com>
2026-08-05 21:13:24 +0200

    fix(k9s): correct plugin structure and update scopes for force-reconcile-kustomization

--- home/k9s.nix
+++ home/k9s.nix
@@ -9,60 +9,58 @@
   programs.k9s = {
     enable = true;
     plugins = {
-      plugins = {
-        debug = {
-          shortCut = "Shift-D";
-          description = "Add debug container";
-          dangerous = true;
-          scopes = [ "containers" ];
-          command = "kubectl";
-          background = false;
-          confirm = true;
-          args = [
-            "debug"
-            "-it"
-            "--context"
-            "$CONTEXT"
-            "-n=$NAMESPACE"
-            "$POD"
-            "--target=$NAME"
-            "--image=nicolaka/netshoot:v0.12"
-            "--share-processes"
-            "--profile=general"
-            "--"
-            "bash"
-          ];
-        };
-        force-sync-externalsecret = {
-          shortCut = "Ctrl-F";
-          description = "Force sync ExternalSecret";
-          scopes = [ "externalsecrets" ];
-          command = "kubectl";
-          background = false;
-          args = [
-            "annotate"
-            "externalsecret"
-            "$NAME"
-            "force-sync=$MOMENT"
-            "--overwrite"
-            "-n=$NAMESPACE"
-          ];
-        };
-        force-reconcile-kustomization = {
-          shortCut = "Ctrl-R";
-          description = "Force reconcile Flux Kustomization";
-          scopes = [ "pods" ];
-          command = "kubectl";
-          background = false;
-          args = [
-            "annotate"
-            "kustomization"
-            "$NAME"
-            "reconcile.fluxcd.io/requestedAt=$MOMENT"
-            "--overwrite"
-            "-n=$NAMESPACE"
-          ];
-        };
+      debug = {
+        shortCut = "Shift-D";
+        description = "Add debug container";
+        dangerous = true;
+        scopes = [ "containers" ];
+        command = "kubectl";
+        background = false;
+        confirm = true;
+        args = [
+          "debug"
+          "-it"
+          "--context"
+          "$CONTEXT"
+          "-n=$NAMESPACE"
+          "$POD"
+          "--target=$NAME"
+          "--image=nicolaka/netshoot:v0.12"
+          "--share-processes"
+          "--profile=general"
+          "--"
+          "bash"
+        ];
+      };
+      force-sync-externalsecret = {
+        shortCut = "Ctrl-F";
+        description = "Force sync ExternalSecret";
+        scopes = [ "externalsecrets" ];
+        command = "kubectl";
+        background = false;
+        args = [
+          "annotate"
+          "externalsecret"
+          "$NAME"
+          "force-sync=$MOMENT"
+          "--overwrite"
+          "-n=$NAMESPACE"
+        ];
+      };
+      force-reconcile-kustomization = {
+        shortCut = "Shift-R";
+        description = "Force reconcile Flux Kustomization";
+        scopes = [ "kustomizations" ];
+        command = "kubectl";
+        background = false;
+        args = [
+          "annotate"
+          "kustomization"
+          "$NAME"
+          "reconcile.fluxcd.io/requestedAt=$MOMENT"
+          "--overwrite"
+          "-n=$NAMESPACE"
+        ];
       };
     };
   };
