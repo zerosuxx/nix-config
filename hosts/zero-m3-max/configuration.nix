e36d1a72a306679b2a7b2ad0ac7db8cc844f6f88
Tamas Mohos <zerosuxx@gmail.com>
2026-09-11 11:02:09 +0200

    fix: correct platform check for aarch64-darwin in extraOptions

--- hosts/zero-m3-max/configuration.nix
+++ hosts/zero-m3-max/configuration.nix
@@ -12,7 +12,7 @@ in
     extraOptions = ''
       auto-optimise-store = true
       experimental-features = nix-command flakes
-    '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+    '' + lib.optionalString (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") ''
       extra-platforms = x86_64-darwin aarch64-darwin
     '';
   };
